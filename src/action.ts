be513f0de36924a70985c9ea1dd9512275c241c3
Mqx <62719703+Mqxx@users.noreply.github.com>
2026-09-18 09:38:56 +0200

    Update action.ts

--- src/action.ts
+++ src/action.ts
@@ -1,12 +1,6 @@
-import actionsCore from '@actions/core';
-import * as yaml from '@std/yaml'
+import * as actionsCore from '@actions/core';
 
-const customTextInput = actionsCore.getInput('custom-text-input');
-const customListInput = yaml.parse(actionsCore.getInput('custom-list-input'));
-const customKeyValueInput = yaml.parse(actionsCore.getInput('custom-key-value-input'));
+const pathInput = actionsCore.getInput('path');
 
-console.log('custom-text-input:', customTextInput);
-console.log('custom-list-input:', customListInput);
-console.log('custom-key-value-input:', customKeyValueInput);
+console.log('path:', pathInput);
 
-actionsCore.setOutput('custom-text-output', 'Test123');
